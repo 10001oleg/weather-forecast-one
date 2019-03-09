4c8c84adaf37d748582d96ff299c9606232d3512
10001oleg <10001oleg@gmail.com>
2019-03-09 11:39:54 -0800

    Updates

---
+++ precache-manifest.1d2ae2ac0e99dd717f9bec9790399a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5c6767df03f152f24d",
+    "url": "/weather-forecast-one/static/css/main.62b1d91a.chunk.css"
+  },
+  {
+    "revision": "5d5c6767df03f152f24d",
+    "url": "/weather-forecast-one/static/js/main.3666dc1b.chunk.js"
+  },
+  {
+    "revision": "3b2ad7ec6c00fb312fd6",
+    "url": "/weather-forecast-one/static/js/runtime~main.dcdeb6ce.js"
+  },
+  {
+    "revision": "a49d0019cff28283d2a7",
+    "url": "/weather-forecast-one/static/js/2.d3764b9e.chunk.js"
+  },
+  {
+    "revision": "0173f471050f03babbf6e7b682a79229",
+    "url": "/weather-forecast-one/index.html"
+  }
+];
